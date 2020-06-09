231fe0a069a5efe62a9eaa9d3678cec8baf5244d
zivuch <zivuch@gmail.com>
2020-06-09 20:27:46 +0300

    ajax

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -9,7 +9,7 @@ xhr.onload = function(){
     console.log(`Error: ${xhr.status}: ${xht.statusText}`);
   }
   else{
-    console.log(xhr.response);
+    built(xhr.response);
   }
 }
 
@@ -24,3 +24,25 @@ xhr.onload = function(){
 xhr.onerror = function() {
   console.log('Error something wrong');
 };
+
+
+const built = (arr) => {
+  const root = document.getElementById('root');
+  arr.forEach((item, i) => {
+    let div = document.createElement('div');
+    let h1 = document.createElement('h3')
+    let p = document.createElement('p');
+    let img = document.createElement('img');
+
+    h1.innerText = item.name;
+    p.innerText = item.company.name;
+    img.setAttribute('src', `https://robohash.org/${item.id}?200x200`)
+
+    div.appendChild(img);
+    div.appendChild(h1);
+    div.appendChild(p);
+
+    root.appendChild(div);
+  });
+
+}
