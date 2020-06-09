8dfc425d1ad628a78f231579004deb7ccd556c9e
zivuch <zivuch@gmail.com>
2020-06-09 14:14:11 +0300

    ajax

---
+++ newajax/ajax.js
@@ -0,0 +1,27 @@
+let xhr = new XMLHttpRequest();
+
+xht.open('GET','https://zivuch.github.io/newajax/data.html');
+
+xhr.send();
+
+xhr.onload = function() {
+  if(xhr.status != 200) {
+    console.log(`Error: ${xhr.status}: ${xhr.statusText}`);
+  }
+  else{
+    console.log(`Done, got ${xhr.response.length} bytes`); // response is the server
+    console.log(xhr.response)
+  }
+}
+
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    console.log(`Received ${event.loaded} of ${event.total} bytes`);
+  } else {
+    console.log(`Received ${event.loaded} bytes`); // no Content-Length
+  }
+};
+
+xhr.onerror = function() {
+  alert("Request failed");
+};
